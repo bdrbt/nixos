9d3587bfb17ae8146decd1ff5f567eb5d85444c5
bdrbt <bdrbt.com@gmail.com>
2025-04-10 18:35:02 +0500

    plymouth boot

--- hosts/rough/boot.nix
+++ hosts/rough/boot.nix
@@ -1,8 +1,37 @@
-{...}: {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.availableKernelModules = ["xhci_pci" "nvme" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-intel" "i915"];
-  boot.extraModulePackages = [];
+{pkgs,...}: {
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "rings";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "rings" ];
+        })
+      ];
+    };
+
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+
+    initrd = {
+      availableKernelModules = ["xhci_pci" "nvme" "usb_storage" "sd_mod"];
+      kernelModules = [];
+      verbose = false;
+    };
+    consoleLogLevel = 3;
+
+    kernelModules = ["kvm-intel" "i915"];
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+      "i915.modeset=1"
+      "vt.global_cursor_default=0"
+    ];
+
+    extraModulePackages = [];
+  };
 }
